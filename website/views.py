1e4fcd03634b62f969636ff61143600b26c7203f
evankellener <crazyevan@kellener.com>
2024-03-03 11:21:52 -0800

    Almost done

--- website/views.py
+++ website/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-
+from .forms import SignUpForm   
+from .models import Record
 
 # Create your views here.
 
 def home(request):
+    records = Record.objects.all()
     if request.method == "POST":
         username = request.POST.get('username')
         password = request.POST.get('password')
@@ -18,7 +20,7 @@ def home(request):
             messages.success(request, "Either username or password is incorrect")  # Set error message only if authentication fails
             return render(request, 'home.html', {})
     else:
-        return render(request, 'home.html', {})
+        return render(request, 'home.html', {'records':records})
     
 def logout_user(request):
     logout(request)
@@ -26,4 +28,25 @@ def logout_user(request):
     return redirect('home')
 
 def register_user(request):
-    return render(request, 'register.html', {})
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username = username, password = password)
+            login(request, user)
+            messages.success(request, "You have successfully registered")
+            return redirect('home')
+    else:
+        form = SignUpForm()
+        return render(request, 'register.html', {'form':form})
+    return render(request, 'register.html', {'form':form})
+
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+        customer_record = Record.objects.get(id = pk) 
+        return render(request, 'record.html', {'customer_record':customer_record})
+    else:
+        messages.success(request, "You must be logged in to view customer records")
+        return redirect('home')
